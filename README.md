8741cd6603766fd90eb59fc63b7d7066968ffdb2
Pierre Constantineau <jpconstantineau@gmail.com>
2023-03-13 17:22:42 -0600

    updated and split functions

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Dupe-Finder
-testing with nodejs
+
+## Goal 
+Find Duplicates from backups stored in multiple folders.
+
+## How
+
+- Scan solders and store the file hash information in a database
+- query the database for duplicates - starting with the largest sizes and/or the highest number of duplicates
+- query the database for duplicates files with different names
--- findfiles.js
+++ findfiles.js
@@ -1,35 +1,89 @@
-const { readdirSync, statSync , readFileSync} = require('fs');
+const os = require("os");
+const { createReadStream, readdirSync, statSync , readFileSync} = require('fs');
 const crypto = require('crypto');
 
-const getFileList = (dirName) => {
+
+
+// ---------------------------------------------------
+const resultHandlerLogger = (data) => {
+    console.log(data);
+}
+
+// ---------------------------------------------------
+const getFileDetails = (dirName, fileName, paramminsize, fcnresultcall) => {                
+    const hostname = os.hostname();
+    const stats = statSync(`${dirName}/${fileName}`);
+    if (stats.size > paramminsize)
+    {
+        var fd = createReadStream(`${dirName}/${fileName}`);
+        var hashSum = crypto.createHash('sha512');
+        hashSum.setEncoding('hex');
+
+        var path2 = dirName.split('/');
+        path2 = path2[path2.length-1];
+        var hashSumpath = crypto.createHash('sha512');
+        hashSumpath.update(path2);
+        var hexpath = hashSumpath.digest('hex');
+
+        fd.on('end', function() {
+            hashSum.end();
+            hex = hashSum.read();
+            const data = { pathsha256:hexpath,
+                filesha256: hex,
+                hostname: hostname,
+                folder: dirName,
+                name: fileName,
+                size:  stats.size,
+                atime: stats.atime,
+                mtime: stats.mtime,
+                ctime: stats.ctime
+              };
+
+              fcnresultcall(data);
+        });
+
+        fd.pipe(hashSum);
+    }
+
+
+}
+
+// ---------------------------------------------------
+const getFileList = (dirName,paramminsize,fcnlogger) => {
     let filecount = 0;
     const items = readdirSync(dirName, { withFileTypes: true });
-
     for (const item of items) {
         if (item.isDirectory()) {
-            filecount = filecount + getFileList(`${dirName}/${item.name}`);
+            filecount = filecount + getFileList(`${dirName}/${item.name}`,paramminsize, fcnlogger);
         } else {
             if (item.isFile())
             {
-                var stats = statSync(`${dirName}/${item.name}`);
                 filecount++;
-             
-                const fileBuffer = readFileSync(`${dirName}/${item.name}`);
-                const hashSum = crypto.createHash('sha256');
-                hashSum.update(fileBuffer);
-
-                const hex = hashSum.digest('hex');
-
-                console.log( hex  + ' ' + stats.size + ' ' + item.name  );
-
+                getFileDetails (`${dirName}`,`${item.name}`,paramminsize, fcnlogger);
             }
-            
         }
     }
-
     return filecount;
 };
 
-const files = getFileList('.');
+// ---------------------------------------------------
+var args = process.argv.slice(2);
+paramminsize=1;
+parampath=".";
 
-console.log(files);
+if (args.length>1)
+{
+    paramminsize=args[1];
+
+}
+if (args.length>0)
+{
+    parampath=args[0];
+
+}
+
+//console.log(args);
+
+const filecount = getFileList(parampath,paramminsize,resultHandlerLogger);
+
+console.log(filecount);
