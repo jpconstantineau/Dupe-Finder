06540b0417ef72a78c31589016a0a9fedfb24002
Pierre Constantineau <jpconstantineau@gmail.com>
2023-04-10 16:41:03 -0600

    adding rate/promise concurrency limiter

--- scanfiles.js
+++ scanfiles.js
@@ -1,31 +1,36 @@
-const os = require("os");
-const fs = require('fs');
-const path = require('path');
-const crypto = require('crypto');
+import os from "os";
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
 
-var config = require('./config.json');
+import { promisify } from 'util';
+import pLimit from 'p-limit' ;
 
-const protocol = 'http'
-const https  = require(protocol);
+const concurrencyLimit = os.cpus().length; // set the concurrency limit to number of cpu cores
+
+const limiter = pLimit(concurrencyLimit);
+
+import { readFile } from 'fs/promises';
+const config = JSON.parse(
+  await readFile(
+    new URL('./config.json', import.meta.url)
+  )
+);
+
+ 
+//import https  from 'https';
+import http  from 'http';
+
+
+const protocol = http;
 
 var args = process.argv.slice(2);
-paramminsize=1;
-parampath=".";
+const paramminsize = args.length > 1 ? args[1] : 1;
+const parampath = args.length > 0 ? args[0] : '.';
 
 // ---------------------------------------------------
 
-if (args.length>1)
-{
-    paramminsize=args[1];
-}
-if (args.length>0)
-{
-    parampath=args[0];
-}
-
-// ---------------------------------------------------
-
-resultHandlerLogger = (data) => {
+const resultHandlerLogger = (data) => {
     console.log((data));
     return data;
 }
@@ -46,9 +51,7 @@ const resultHandlerPosttoDatabase = (requestData) => {
       }
     };
 
-    const request = https.request(options, (res) => {
-      //console.log(`STATUS: ${res.statusCode}`);
-      //console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    const request = protocol.request(options, (res) => {
       res.setEncoding('utf8');
       if (res.statusCode !== 201) {
         console.error(`Did not get a 201 - Created from the server. Code: ${res.statusCode}`);
@@ -56,8 +59,7 @@ const resultHandlerPosttoDatabase = (requestData) => {
         return;
       }
     
-      let data = '';
-              
+      let data = '';              
         res.on('data', (chunk) => {
           data += chunk;
         });
@@ -80,6 +82,14 @@ const resultHandlerPosttoDatabase = (requestData) => {
 
 // ---------------------------------------------------
 
+function getSubfolderHash(filePath) {
+  const subfolders = filePath.split(path.sep);
+  const lastSubfolder = (subfolders.length>1) ? subfolders[subfolders.length - 2] : '';
+  var hashSumpath = crypto.createHash('sha512');
+  hashSumpath.update(lastSubfolder);
+  var hexpath = hashSumpath.digest('hex');
+  return (hexpath);
+}
 
 
 function getFilePaths(directoryPath, minSize) {
@@ -91,81 +101,75 @@ function getFilePaths(directoryPath, minSize) {
     if (stat.isDirectory()) {
       filePaths = filePaths.concat(getFilePaths(fullPath, minSize));
     } else if (stat.isFile() && stat.size >= minSize) {
-      subfolderHash = getSubfolderHash(fullPath); 
+      var subfolderHash = getSubfolderHash(fullPath); 
       filePaths.push({filePath: fullPath, subfolderHash});
     }
   });
   return filePaths;
 }
 
-function getFileStats(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.stat(filePath, (error, stats) => {
-      if (error) {
-        reject(error);
-      } else {
-        const fileStats = {
-          path: path.dirname(filePath),
-          name: path.basename(filePath),
-          extension: path.extname(filePath),
-          size:  stats.size,
-          created: stats.birthtime,
-          modified: stats.mtime,
-          accessed: stats.atime
-        };
-        resolve(fileStats);
-      }
-    });
-  });
+
+
+async function getFileStats(filePath) {
+  const stat = await promisify(fs.stat)(filePath);
+  return {
+    path: path.dirname(filePath),
+    name: path.basename(filePath),
+    extension: path.extname(filePath),
+    size: stat.size,
+    created: stat.birthtime,
+    modified: stat.mtime,
+    accessed: stat.atime
+  };
 }
 
 
-function getHash(filePath) {
-  return new Promise((resolve, reject) => {
-    const hash = crypto.createHash('sha512');
-    const stream = fs.createReadStream(filePath);
+async function getHash(filePath) {
+  const hash = crypto.createHash('sha512');
+  const stream = fs.createReadStream(filePath);
 
+  await new Promise((resolve, reject) => {
     stream.on('data', chunk => hash.update(chunk));
     stream.on('error', error => reject(error));
-    stream.on('end', () => resolve(hash.digest('hex')));
+    stream.on('end', () => resolve());
   });
+
+  return hash.digest('hex');
 }
 
-function getSubfolderHash(filePath) {
-  const subfolders = filePath.split(path.sep);
-  const lastSubfolder = (subfolders.length>1) ? subfolders[subfolders.length - 2] : '';
-  var hashSumpath = crypto.createHash('sha512');
-  hashSumpath.update(lastSubfolder);
-  var hexpath = hashSumpath.digest('hex');
-  return (hexpath);
+
+async function processFiles(filePath, subfolderHash, hostname) {
+  try {
+    const hash = await getHash(filePath);
+    const fileStats = await getFileStats(filePath);
+    const message = {
+      hostname,
+      path: fileStats.path,
+      name: fileStats.name,
+      extension: fileStats.extension,
+      size: fileStats.size,
+      created: fileStats.created,
+      modified: fileStats.modified,
+      accessed: fileStats.accessed,
+      hash,
+      subfolderHash
+    };
+    resultHandlerPosttoDatabase(message);
+  } catch (error) {
+    console.error(`Error processing ${filePath}: ${error.message}`);
+  }
 }
 
 async function scanFiles(directoryPath, minSize) {
   const filePaths = getFilePaths(directoryPath, minSize);
   console.log('scanFiles');
   const hostname = os.hostname();
-  for (const {filePath, subfolderHash} of filePaths) {
-    try {
-      const hash = await getHash(filePath);
-      const fileStats = await getFileStats(filePath);
-      const message = {
-        hostname,
-        path: fileStats.path,
-        name: fileStats.name,
-        extension: fileStats.extension,
-        size: fileStats.size,
-        created: fileStats.created,
-        modified: fileStats.modified,
-        accessed: fileStats.accessed,
-        hash,
-        subfolderHash
-      };
-      resultHandlerPosttoDatabase(message);
-    } catch (error) {
-      console.error(`Error processing ${filePath}: ${error.message}`);
-    }
-  }
-
+  const promises = filePaths.map(({ filePath, subfolderHash }) =>
+    limiter(() =>
+      processFiles(filePath, subfolderHash, hostname)
+    )
+  );
+  await Promise.all(promises); // wait for all promises to complete
 }
 
 scanFiles(parampath,paramminsize); // Minimum size of 1kB
